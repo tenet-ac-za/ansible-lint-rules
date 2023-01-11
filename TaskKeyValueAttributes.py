2a8251b9fb5733f0317f55b4734c87ec67fa7d02
Guy Halse <guy@tenet.ac.za>
2023-01-11 17:27:25 +0200

    Update for ansible-lint 6

--- TaskKeyValueAttributes.py
+++ TaskKeyValueAttributes.py
@@ -1,28 +1,47 @@
 # Copyright 2021, Tertiary Education and Research Network of South Africa
-import ansiblelint.utils
-from ansiblelint.rules import AnsibleLintRule
+from __future__ import annotations
+
 import re
+import sys
+from typing import TYPE_CHECKING
+
+from ansiblelint.rules import AnsibleLintRule
+from ansiblelint.constants import PLAYBOOK_TASK_KEYWORDS, ANNOTATION_KEYS, LINE_NUMBER_KEY, INCLUSION_ACTION_NAMES, ROLE_IMPORT_ACTION_NAMES
+
+if TYPE_CHECKING:
+    from typing import Any
+    from ansiblelint.errors import MatchError
+    from ansiblelint.file_utils import Lintable
 
 '''
 Attempt to find the old key=value form of tasks
 '''
 class TaskKeyValueAttributes(AnsibleLintRule):
-    id = 'tenet-keyvalue'
+    id = 'tenet'
     shortdesc = 'Uses legacy key=value attribute format'
     description = (
         'The old key=value attribute format makes tasks '
         'difficult to read and debug. Consider using a YAML dict'
     )
     severity = 'LOW'
-    tags = ['tenet']
+    tags = ['formatting']
     version_added = 'v4.3.7'
 
     keyvalue_regex = re.compile(r"(?:\s+|^)[^=]+=[^=]+(?:\s+|$)")
 
-    def matchplay(self, file, play):
+    VALID_KEYS = [
+        'name', 'action', 'when', 'async', 'poll', 'notify', 'first_available_file', 'include',
+        'include_tasks', 'import_tasks', 'import_playbook', 'tags', 'register', 'ignore_errors',
+        'delegate_to', 'local_action', 'transport', 'remote_user', 'sudo', 'sudo_user',
+        'sudo_pass', 'when', 'connection', 'environment', 'args', 'any_errors_fatal',
+        'changed_when', 'failed_when', 'check_mode', 'delay', 'retries', 'until', 'su',
+        'su_user', 'su_pass', 'no_log', 'run_once', 'become', 'become_user', 'become_method',
+    ]
+
+    def matchplay(self, file: Lintable, play: dict[str, Any]) -> list[MatchError]:
         errors = []
-        if not file['type'] in ['tasks', 'handlers']:
-            return False
+        if not file.kind in PLAYBOOK_TASK_KEYWORDS:
+            return []
 
         if isinstance(play, dict):
             if 'block' in play:
@@ -31,20 +50,28 @@ class TaskKeyValueAttributes(AnsibleLintRule):
                 self.matchplay(file, play['tasks'])
             else:
                 for k, v in play.items():
-                    if k in ansiblelint.utils.VALID_KEYS:
-                        continue
-                    elif k in ['skipped_rules', '__ansible_action_type__', ansiblelint.utils.LINE_NUMBER_KEY, ansiblelint.utils.FILENAME_KEY]:
+                    if (k in self.VALID_KEYS
+                            or k in ANNOTATION_KEYS
+                            or k in INCLUSION_ACTION_NAMES
+                            or k in ROLE_IMPORT_ACTION_NAMES
+                        ):
                         continue
                     elif k in ['set_fact']:
                         continue
                     elif k.startswith("with_"):
                         continue
-                    elif k.startswith("include_") or k.startswith("import_"):
-                        continue
                     elif not isinstance(play[k], str):
                         continue
                     if self.keyvalue_regex.match(play[k]):
-                        errors.append((k + ": " + play[k], self.shortdesc))
+                        errors.append(
+                            self.create_matcherror(
+                                message=self.shortdesc,
+                                filename=file,
+                                tag=f"{self.id}[keyvalue]",
+                                details=f"{k}: {v}",
+                                linenumber=play[LINE_NUMBER_KEY],
+                            )
+                        )
 
         if isinstance(play, list):
             for play_item in play:
